4d3141adb426d5334e4512946f9149e2ee4324fc
SANGCO <nsg1213@gmail.com>
2019-01-21 13:12:26 +0900

    ThePalindrome 주석 추가

--- src/main/java/TopCoder_04/ThePalindrome.java
+++ src/main/java/TopCoder_04/ThePalindrome.java
@@ -10,7 +10,9 @@ public class ThePalindrome {
 
             for (int j = 0; j < s.length(); j++) {
 
-                //반대쪽에 문자가 존재하는 데 다를 경우 플래그를 변경합니다.
+                // 반대쪽에 문자가 존재하는 데 다를 경우 플래그를 변경합니다.
+                // 최악은 6글자면 뒤에 앞에 5글자를 붙이는 경우다.
+                // for문을 돌면서 최악의 상황보다 빨리 회문이 되는 경우를 찾는다.
                 if ((i - j - 1) < s.length() && s.charAt(j) != s.charAt(i - j - 1)) {
                     flag = false;
                     break;
