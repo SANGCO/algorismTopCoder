0d3650d3614f9be961130068d1dbc1b71c5871dd
SANGCO <nsg1213@gmail.com>
2019-01-21 12:59:19 +0900

    junit 테스트 세팅

---
+++ src/test/java/TopCoder_04/ThePalindromeTest.java
@@ -0,0 +1,37 @@
+package TopCoder_04;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ThePalindromeTest {
+
+    private ThePalindrome thePalindrome;
+
+    @Before
+    public void setUp() {
+      thePalindrome = new ThePalindrome();
+    }
+
+    @Test
+    public void case_01() {
+      assertEquals(thePalindrome.find("abab"), 5);
+    }
+
+    @Test
+    public void case_02() {
+        assertEquals(thePalindrome.find("abacaba"), 7);
+    }
+
+    @Test
+    public void case_03() {
+        assertEquals(thePalindrome.find("qwerty"), 11);
+    }
+
+    @Test
+    public void case_04() {
+        assertEquals(thePalindrome.find("abdfhdyrbdbsdfghjkllkjhgfds"), 38);
+    }
+
+}
